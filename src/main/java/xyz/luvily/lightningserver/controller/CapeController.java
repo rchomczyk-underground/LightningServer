71d0b0b27e13ad60d7a642ea38feeb42a6832dea
luvily <ra.chomczyk@gmail.com>
2021-03-15 16:43:26 +0100

    :broom: Cleaning up.

--- src/main/java/xyz/luvily/lightningserver/controller/CapeController.java
+++ src/main/java/xyz/luvily/lightningserver/controller/CapeController.java
@@ -1,19 +1,18 @@
 package xyz.luvily.lightningserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import xyz.luvily.lightningserver.model.Cape;
 import xyz.luvily.lightningserver.model.User;
 import xyz.luvily.lightningserver.service.CapeService;
 import xyz.luvily.lightningserver.service.UserService;
 
-@RestController
+@RestController()
+@RequestMapping("/capes")
 public class CapeController {
 
     private final CapeService capeService;
@@ -26,30 +25,36 @@ public class CapeController {
     }
 
     @CrossOrigin
-    @GetMapping(value = "/capes/texture/{identifier}", produces = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity<byte[]> getSpecifiedCape(@PathVariable int identifier) {
+    @GetMapping( "/texture/{identifier}")
+    public ResponseEntity<Resource> getSpecifiedCape(@PathVariable int identifier) {
         Cape cape = capeService.getCape(identifier);
         if (cape == null) {
-            return new ResponseEntity<>(new byte[0], HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
 
-        return new ResponseEntity<>(cape.getTexture(), HttpStatus.OK);
+        ByteArrayResource resource = new ByteArrayResource(cape.getTexture());
+        return ResponseEntity.ok()
+                .contentLength(resource.contentLength())
+                .contentType(MediaType.IMAGE_PNG)
+                .body(resource);
     }
 
-    @GetMapping(value = "/capes/{raw}", produces = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity<byte[]> getUserCape(@PathVariable String raw) {
-        String username = raw.replace(".png", "");
-
+    @GetMapping("/{username}.png")
+    public ResponseEntity<Resource> getUserCape(@PathVariable String username) {
         User user = userService.getUser(username);
         if (user == null) {
-            return new ResponseEntity<>(new byte[0], HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
 
         Cape cape = capeService.getCape(user.getCape());
         if (cape == null) {
-            return new ResponseEntity<>(new byte[0], HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
 
-        return new ResponseEntity<>(cape.getTexture(), HttpStatus.OK);
+        ByteArrayResource resource = new ByteArrayResource(cape.getTexture());
+        return ResponseEntity.ok()
+                .contentLength(resource.contentLength())
+                .contentType(MediaType.IMAGE_PNG)
+                .body(resource);
     }
 }
--- src/main/java/xyz/luvily/lightningserver/controller/UserController.java
+++ src/main/java/xyz/luvily/lightningserver/controller/UserController.java
@@ -3,14 +3,17 @@ package xyz.luvily.lightningserver.controller;
 import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.luvily.lightningserver.model.User;
 import xyz.luvily.lightningserver.service.UserService;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -20,17 +23,22 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users/{username}")
+    @GetMapping("/{username}")
     public ResponseEntity<String> getUser(@PathVariable String username) {
         User user = userService.getUser(username);
         if (user == null) {
-            return new ResponseEntity<>("", HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
         }
 
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("username", user.getUsername());
         jsonObject.addProperty("cape", user.getCape());
 
-        return new ResponseEntity<>(jsonObject.toString(), HttpStatus.OK);
+        String jsonString = jsonObject.toString();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .contentLength(jsonString.length())
+                .body(jsonString);
     }
 }
